e6511ded331332cc7de5b0a2d8a6c2f6ff63b797
Synaric <gcy2014@126.com>
2019-02-14 11:41:23 +0800

    update samples

--- app/src/main/java/com/synaric/myarts/MainActivity.kt
+++ app/src/main/java/com/synaric/myarts/MainActivity.kt
@@ -3,6 +3,7 @@ package com.synaric.myarts
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.synaric.myarts.samples.kotlin.clz.ClzAndImplementation
+import com.synaric.myarts.samples.kotlin.clz.DataClz
 import com.synaric.myarts.samples.kotlin.clz.Extensions
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -16,7 +17,8 @@ class MainActivity : AppCompatActivity() {
         sample_text.text = stringFromJNI()
 
         // ClzAndImplementation.run()
-        Extensions.run()
+        // Extensions.run()
+        DataClz.run()
     }
 
     /**
